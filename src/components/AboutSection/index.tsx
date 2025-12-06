e51d05afbbbfc8320347857e4474407859321de6
BaziotaBeans <1000017079@ucan.edu>
2025-12-06 16:30:55 +0100

    feat: add TextHoverEffect

--- src/components/AboutSection/index.tsx
+++ src/components/AboutSection/index.tsx
@@ -1,8 +1,10 @@
+import { TextHoverEffect } from "../animated/TextHoverEffect";
+
 export function AboutSection() {
   return (
     <section className="w-full h-screen relative">
       <div className="absolute top-36 w-full flex flex-col items-center justify-center">
-        <h2 className="text-4xl font-bold">About</h2>
+        <TextHoverEffect text="Beans" />
       </div>
     </section>
   );
