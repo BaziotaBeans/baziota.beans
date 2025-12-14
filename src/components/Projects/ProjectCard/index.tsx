f5507ff8356e6dd6e1f8f27c77b94eeab4ef4ce4
BaziotaBeans <1000017079@ucan.edu>
2025-12-14 16:49:08 +0100

    feat: update projectCatd

--- src/components/Projects/ProjectCard/index.tsx
+++ src/components/Projects/ProjectCard/index.tsx
@@ -1,8 +1,7 @@
 "use client";
 
-import TechBadges from "@/components/TechBadges";
 import Image from "next/image";
-import { TechBadge } from "./TechBage";
+import { Globe } from "lucide-react";
 import Link from "next/link";
 
 export interface Props {
@@ -23,7 +22,7 @@ export function ProjectCard({
   tags,
 }: Props) {
   return (
-    <div className="w-full h-full flex flex-col border border-gray-800 rounded-lg p-4">
+    <div className="w-full h-full flex flex-col border border-gray-800 rounded-lg p-4 transition-all duration-500 hover:border-green-400/20 group">
       <span className="text-xs text-gray-400">{year}</span>
       <h3 className="text-lg font-bold text-gray-300">{name}</h3>
       <p className="font-medium text-sm text-gray-400">{description}</p>
@@ -34,7 +33,7 @@ export function ProjectCard({
           width={800}
           height={600}
           quality={100}
-          className="w-full h-full object-cover"
+          className="w-full h-full object-cover transition-all duration-500 group-hover:-translate-y-2 group-hover:scale-105"
         />
       </div>
 
@@ -49,11 +48,17 @@ export function ProjectCard({
         ))}
       </div>
 
-      <div className="flex items-center justify-between gap-2 mt-auto">
-        <span className="text-xs text-gray-400 hover:text-gray-300 transition-colors">
+      <Link
+        href={link}
+        target="_blank"
+        className="flex items-center justify-between gap-2 mt-auto group transition-all duration-500 cursor-pointer"
+      >
+        <span className="text-xs text-gray-400 transition-colors group-hover:text-green-400">
           Live Site
         </span>
-      </div>
+
+        <Globe className="w-4 h-4 text-gray-400 transition-all duration-500 group-hover:text-gray-200 group-hover:rotate-180" />
+      </Link>
     </div>
   );
 }
