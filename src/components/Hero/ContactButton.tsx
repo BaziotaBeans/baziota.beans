3b50e076199d9430156fab7cbec13660583d988b
BaziotaBeans <1000017079@ucan.edu>
2025-12-25 18:45:01 +0100

    feat: update intl

--- src/components/Hero/ContactButton.tsx
+++ src/components/Hero/ContactButton.tsx
@@ -1,6 +1,11 @@
+"use client";
+
 import { ArrowIcon } from "./icons/ArrowIcon";
+import { useTranslations } from "next-intl";
 
 export function ContactButton() {
+  const t = useTranslations("Hero");
+
   return (
     <a
       href="mailto:fabiobaziota@gmail.com"
@@ -9,7 +14,7 @@ export function ContactButton() {
       <ArrowIcon className="absolute w-6 fill-white z-9 transition-all duration-700 -left-1/4 group-hover:left-4 group-hover:fill-white" />
 
       <span className="relative z-1 transition-all duration-700 -translate-x-3 group-hover:translate-x-3 font-bold">
-        Let's talk
+        {t("contactButton")}
       </span>
       <span className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-5 h-5 bg-neutral-600 rounded-full opacity-0 transition-all duration-700 group-hover:w-[220px] group-hover:h-[220px] group-hover:opacity-100"></span>
       <ArrowIcon className="absolute w-6 fill-neutral-600 z-9 transition-all duration-700 right-4 group-hover:-right-1/4 group-hover:fill-[#212121]" />
